0efb758c15127f68419b71cb0a5d39b75cb7f49b
mahmud <mahmudskarim@gmail.com>
2020-08-14 15:22:32 -0400

    finished bascic header

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Header from './components/Header'
 function App() {
   return (
     <div className="App">
-      <h1>Let's do this</h1>
       <Header />
-      dfdf
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from '@material-ui/core/IconButton';
 
 function Header() {
     return (
-        <div>
-            <PersonIcon />
-            <h2>Header</h2>
+        // BEM
+        <div className="header">
+            <IconButton>
+                <PersonIcon className="header__icon" fontSize="large" />
+            </IconButton>
+            <img
+                className="header__logo"
+                src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+                alt="tinder logo"
+            />
+            <IconButton>
+                <ForumIcon className="header__icon" fontSize="large" />
+            </IconButton>
         </div>
     )
 }
